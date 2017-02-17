ce5ba1735fef7f37af0add43cbedd8a383e809f5
Chilipp <p.sommer@posteo.de>
2017-02-17 21:13:21 +0100

    Minor changes

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -13,7 +13,6 @@ SHA=`git rev-parse --verify HEAD`
 cd $SRC_DIR
 
 REPO_NAME=$(basename `git config remote.origin.url`)
-REPO_NAME=${REPO_NAME%.git}
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis"
