046a472c5d9ce2a708b44c69e2b88855730256a4
Chilipp <p.sommer@posteo.de>
2017-02-22 23:35:48 +0100

    Replace git pull by git fetch && git merge

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -45,8 +45,9 @@ git checkout $TARGET_BRANCH
 set +ex
 
 for COUNTER in {1..10} ; do
-    echo Try No. ${COUNTER}: "git pull && git rebase TRAVIS_DEPLOY && git push https://<secure>@${REPO_NAME} $TARGET_BRANCH"
-    git pull "https://${GH_REPO_TOKEN}@${REPO_NAME}" $TARGET_BRANCH
+    echo Try No. ${COUNTER}: "git fetch && git merge && git rebase TRAVIS_DEPLOY && git push https://<secure>@${REPO_NAME} $TARGET_BRANCH"
+    git fetch "https://${GH_REPO_TOKEN}@${REPO_NAME}" $TARGET_BRANCH
+    git merge -m " Merge branch '${TRAVIS_BRANCH}' of ${REPO}"
     git rebase TRAVIS_DEPLOY
     git push "https://${GH_REPO_TOKEN}@${REPO_NAME}" $TARGET_BRANCH  &> log.txt
     if [[ $? != 0 ]]; then  # push failed, wait 10 seconds and try again
