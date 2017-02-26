9b33c8bafe9153b127f68c69f0ef35c222241ac6
Chilipp <p.sommer@posteo.de>
2017-02-26 02:45:54 +0100

    Fixed bug in subprocess call

--- ci/update_reference_submodule.py
+++ ci/update_reference_submodule.py
@@ -13,9 +13,9 @@ spr.check_call(['git', 'clone', '-b', ref_branch,
                 repo.replace('psy-simple', 'psy-simple-references'),
                 deploy_dir])
 
-spr.check_call(['git branch TRAVIS_DEPLOY'.split()])
+spr.check_call('git branch TRAVIS_DEPLOY'.split())
 
-spr.check_call(['git checkout TRAVIS_DEPLOY'.split()])
+spr.check_call('git checkout TRAVIS_DEPLOY'.split())
 
 shutil.copytree(ref_dir, deploy_dir)
 
