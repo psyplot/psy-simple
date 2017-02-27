2c02c187c230776290c8d72c331d72d7ce3c585f
Chilipp <p.sommer@posteo.de>
2017-02-27 14:32:37 +0100

    Removed outdated bash scripts for CI

--- ci/checkout_references.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-# Checkout the correct reference figures branch
-
-REF_REPO=https://github.com/Chilipp/psy-simple-references.git
-
-REFDIR=`python tests/get_ref_dir.py`
-
-git submodule update --init ${REFDIR}
--- ci/set_create_references.sh
+++
@@ -1,6 +0,0 @@
-# This script can be used to create references on travis. It should export a
-# CREATE_REFERENCES variable that, if executed via
-# `bash -c ${CREATE_REFERENCES}` creates the reference figures. Set
-# CREATE_REFERENCES="" if no references shall be created
-
-export CREATE_REFERENCES='tests'
