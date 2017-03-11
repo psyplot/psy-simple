5784c425334cdcc70cc61477f5612682bc111de8
Chilipp <p.sommer@posteo.de>
2017-03-11 02:03:22 +0100

    Added sphinx-autogen command

--- docs/apigen.bash
+++ docs/apigen.bash
@@ -5,4 +5,4 @@ sphinx-apidoc -f -M -e  -T -o api ../psy_simple/
 # replace chapter title in psyplot.rst
 sed -i -e 1,1s/.*/'API Reference'/ api/psy_simple.rst
 
-# sphinx-autogen -o generated *.rst */*.rst
+sphinx-autogen -o generated *.rst 
