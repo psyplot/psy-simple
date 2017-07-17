43aefc49e28d5d6d0cd4aeff525746307810b6db
Chilipp <p.sommer@posteo.de>
2017-07-17 08:58:20 +0200

    Renamed no-removal to no-remove

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 
 def pytest_addoption(parser):
     group = parser.getgroup("psyplot", "psyplot specific options")
-    group.addoption('--no-removal', help='Do not remove created test files',
+    group.addoption('--no-remove', help='Do not remove created test files',
                     action='store_true')
     group.addoption(
         '--ref', help='Create reference figures instead of running the tests',
@@ -9,7 +9,7 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    if config.getoption('no_removal'):
+    if config.getoption('no_remove'):
         import _base_testing
         _base_testing.remove_temp_files = False
     if config.getoption('ref'):
