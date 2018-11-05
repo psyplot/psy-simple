837067a22b17977163eadeb3ce74d48d25cabcd0
Chilipp <p.sommer@posteo.de>
2018-11-05 23:13:17 +0100

    Minor bug fix for conda install

--- ci/install_python.ps1
+++ ci/install_python.ps1
@@ -10,10 +10,10 @@ $BASE_URL = "https://www.python.org/ftp/python/"
 
 function DownloadMiniconda ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
-    if ($python_version -match "3.[456]") {
+    if ($python_version -match "3.[4567]") {
         $filename = "Miniconda3-latest-Windows-" + $platform_suffix + ".exe"
     } else {
-        $filename = "Miniconda-latest-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda2-latest-Windows-" + $platform_suffix + ".exe"
     }
     $url = $MINICONDA_URL + $filename
 
