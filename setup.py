389bcb9f928584337c13509d40f62c93918c1d2b
Philipp Sommer <philipp.sommer@hereon.de>
2021-09-29 16:16:19 +0200

    change license to LGPL and fix disclaimers

--- setup.py
+++ setup.py
@@ -1,3 +1,28 @@
+"""Setup script for the psy-simple package."""
+
+# Disclaimer
+# ----------
+#
+# Copyright (C) 2021 Helmholtz-Zentrum Hereon
+# Copyright (C) 2020-2021 Helmholtz-Zentrum Geesthacht
+# Copyright (C) 2016-2021 University of Lausanne
+#
+# This file is part of psy-simple and is released under the GNU LGPL-3.O license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3.0 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU LGPL-3.0 license for more details.
+#
+# You should have received a copy of the GNU LGPL-3.0 license
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import os.path as osp
 from setuptools import setup, find_packages
@@ -48,18 +73,19 @@ setup(name='psy-simple',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Scientific/Engineering',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
       ],
       keywords='visualization netcdf raster cartopy earth-sciences psyplot',
       url='https://github.com/psyplot/psy-simple',
       author='Philipp S. Sommer',
-      author_email='philipp.sommer@hzg.de',
-      license="GPLv2",
+      author_email='philipp.sommer@hereon.de',
+      license="LGPL-3.0-only",
       packages=find_packages(exclude=['docs', 'tests*', 'examples']),
       install_requires=[
           'psyplot>=1.3.0',
@@ -70,7 +96,7 @@ setup(name='psy-simple',
           osp.join('psy_simple', 'widgets', 'icons', 'cmaps', '*.png'),
           ]},
       project_urls={
-          'Documentation': 'https://psyplot.readthedocs.io/projects/psy-simple',
+          'Documentation': 'https://psyplot.github.io/psy-simple',
           'Source': 'https://github.com/psyplot/psy-simple',
           'Tracker': 'https://github.com/psyplot/psy-simple/issues',
       },
