b164fb41c92bb54e6cec4b7212a9cce56c078ce0
Philipp S. Sommer <philipp.sommer@hereon.de>
2021-10-01 11:25:08 +0200

    Change License to LGPL-3.0-only (#28)
    * change license to LGPL and fix disclaimers
    
    * add changelog for 1.4.0
    
    * fix copyright in docs
    
    * add disclaimer to conf.py
    
    * deploy docs on master branch (not on main)

--- examples/README.rst
+++
@@ -1,5 +0,0 @@
-Example Gallery
-===============
-The psy-simple module provides you some plotters that are designed for very
-simple, non-projected visualization. Those range from simple line plots, over
-violin plots to 2-dimensional plots.
--- psy_simple/widgets/texts.py
+++ psy_simple/widgets/texts.py
@@ -2,7 +2,32 @@
 """Psyplot GUI widgets for modifying label formatoptions
 
 This module contains PyQt widgets that can be used to modify label
-formatoptions (e.g. title, xlabel, titleprops, etc.) in the psyplot GUI."""
+formatoptions (e.g. title, xlabel, titleprops, etc.) in the psyplot GUI.
+"""
+
+# Disclaimer
+# ----------
+#
+# Copyright (C) 2021 Helmholtz-Zentrum Hereon
+# Copyright (C) 2020-2021 Helmholtz-Zentrum Geesthacht
+# Copyright (C) 2016-2021 University of Lausanne
+#
+# This file is part of psy-simple and is released under the GNU LGPL-3.O license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3.0 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU LGPL-3.0 license for more details.
+#
+# You should have received a copy of the GNU LGPL-3.0 license
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from psyplot_gui.compat.qtcompat import (
     QWidget, QComboBox, QHBoxLayout, QPushButton, QLabel, QtGui, with_qt5,
     QToolButton, QIcon, Qt)
